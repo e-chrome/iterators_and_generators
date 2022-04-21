0558e31da95aad45ce61bd9156005ea56eec798e
Евгений Хромов <hromov.gazprom@yandex.ru>
2022-04-21 11:50:35 +0300

    improved iterator

--- iterator.py
+++ iterator.py
@@ -1,19 +1,33 @@
 from nested_list import nested_list
 
+
 class FlatIterator:
-    def __init__(self, list_):
-        self.start = -1
-        self.end = len(list_)
-        self.list_ = list_
+    def __init__(self, some_list):
+        self.main_cursor = 0
+        self.second_cursor = -1
+        self.main_len = len(some_list)
+        self.main_list = some_list
 
     def __iter__(self):
         return self
 
     def __next__(self):
-        self.start += 1
-        if self.start == self.end:
+        if self.main_cursor == self.main_len:
             raise StopIteration
-        return self.list_[self.start]
+        if type(self.main_list[self.main_cursor]) is not list:
+            self.main_cursor += 1
+            self.second_cursor = -1
+            return self.main_list[self.main_cursor - 1]
+
+        self.second_cursor += 1
+        self.second_len = len(self.main_list[self.main_cursor])
+
+        if self.second_cursor == self.second_len:
+            self.main_cursor += 1
+            self.second_cursor = 0
+        if self.main_cursor == self.main_len:
+            raise StopIteration
+        return self.main_list[self.main_cursor][self.second_cursor]
 
 
 def print_items(list_):
